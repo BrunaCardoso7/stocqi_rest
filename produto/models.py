9747a7262adc52723035b7a2860a2052a065051b
BrunaCardoso7 <obitadrawing@gmail.com>
2025-05-06 00:48:53 -0300

    refactor: ajusta model de produto

--- produto/models.py
+++ produto/models.py
@@ -1,16 +1,16 @@
-from django.conf import settings
 from django.db import models
-import uuid
+from django.utils import timezone
 # Create your models here.
 class Produto(models.Model):
-        id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-        nome = models.CharField(null=False, max_length=255)
-        descricao = models.TextField(null=False)
-        beneficios = models.JSONField(default=list)
-        modo_de_uso = models.JSONField(default=list)
-        especificacao = models.JSONField(default=list)
-        indicacao = models.JSONField(default=list)
-        garatia_de_qualidade = models.TextField(null=False)
-        preco_estimado =  models.DecimalField(max_digits=10, decimal_places=2)
-        preco = models.DecimalField(max_digits=10, decimal_places=2)
-        user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="produtos")
+        nm_produto = models.CharField(null=True, max_length=255)
+        ds_produto = models.TextField(null=True)
+        qtd_produto = models.IntegerField(null=True)
+        qtd_min_produto = models.IntegerField(null=True, blank=True)
+        vl_total = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+        dt_create = models.DateTimeField(auto_now_add=True)
+        dt_update =  models.DateTimeField(auto_now=True)
+        dt_deletado = models.DateTimeField(null=True, blank=True)
+        class Meta:
+                db_table = 'produtos'
+                verbose_name = 'Produto'
+                verbose_name_plural = 'Produtos'
